76efc18cecf3d0c3338a2324526f01d422c9187e
kazukousen <mmchari.0228@gmail.com>
2020-03-03 16:57:58 +0900

    compelete 4

--- main.tf
+++ main.tf
@@ -14,3 +14,18 @@ module "dev_server" {
 output "public_dns" {
   value = module.dev_server.public_dns
 }
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
